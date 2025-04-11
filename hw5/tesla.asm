cc216bb48243e5ceadf80918aa878ea752777e0a
williamdam123 <143433814+williamdam123@users.noreply.github.com>
2025-04-10 23:54:18 -0700

    Update tesla.asm

--- hw5/tesla.asm
+++ hw5/tesla.asm
@@ -1,1 +1,11 @@
+; tesla.asm
+global _start
+extern faraday
+extern exit
 
+section .text
+_start:
+    call faraday         ; Call the logic in faraday.asm
+
+    mov edi, 0           ; return 0 to OS
+    call exit            ; exit(0)
